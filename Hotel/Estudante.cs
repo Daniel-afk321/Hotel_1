e3479c179fa2d0185a995ac636ec67dcf149f48e
Daniel-afk321 <83432335+Daniel-afk321@users.noreply.github.com>
2021-10-06 23:10:25 -0300

    Adicionar arquivos de projeto.

---
+++ Hotel/Estudante.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    class Estudante
+    {
+        //metodos
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        //construtor
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+
+    }
+}
---
+++ Hotel/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hospedes que alugarao quartos que é de 1 a 10 quartos  
+            Estudante[] vect = new Estudante[10];
+            //quartos que serão alugados quantidade 
+            Console.WriteLine("Quantos quartos serao alugados");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+             //vai mostrar a ordem de chegada dos hospedes
+                Console.WriteLine($"Hospede #{i}");
+            //nome do hospede
+                Console.WriteLine("Nome");
+                string nome = Console.ReadLine();
+            //Email do hospede
+                Console.WriteLine("Email");
+                string email = Console.ReadLine();
+            //quarto que o hospede vai ficar
+                Console.WriteLine("Quarto");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Estudante(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quarto ocupados");
+            // vai mostrar os quartos que os hospedes escolheram e ja estao ocupado
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+
+
+        }
+    }
+}
